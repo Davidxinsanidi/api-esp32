1be3e5808372ecded514061e043eddd17ea0102d
Davide <juandavid5445@gmail.com>
2026-03-23 22:25:21 -0600

    validacion y errores

--- main.py
+++ main.py
@@ -13,19 +13,21 @@ MONGO_URI = "mongodb+srv://esp32:esp32pass@cluster0.kaqfde1.mongodb.net/iot"
 client = MongoClient(MONGO_URI)
 db = client.iot
 collection = db.sensores
+from pydantic import BaseModel, Field
 
-# Modelo de datos
 class SensorData(BaseModel):
-    temperatura: float
-    humedad: float
-
+    temperatura: float = Field(..., ge=-50, le=100)
+    humedad: float = Field(..., ge=0, le=100)
 @app.get("/")
 def root():
     return {"mensaje": "API funcionando"}
 
 @app.post("/sensor")
 def guardar_sensor(data: SensorData):
-    doc = data.dict()
-    doc["fecha"] = datetime.now()
-    collection.insert_one(doc)
-    return {"status": "dato guardado"} 
+    try:
+        doc = data.dict()
+        doc["fecha"] = datetime.now()
+        collection.insert_one(doc)
+        return {"status": "dato guardado"}
+    except Exception as e:
+        return {"error": str(e)}
